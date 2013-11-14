def12b9edfffe9a5b98fc3d73cdb8c11e54ec4b1
dennis3554077@gmail.com <dennis4079@gmail.com>
2013-11-14 15:12:51 +0000

    add message spam filter

--- modules/spamfilter/spamfilter.controller.php
+++ modules/spamfilter/spamfilter.controller.php
@@ -133,9 +133,42 @@
         }
 
         /**
+		 * @brief The routine process to check the time it takes to store a message, when writing it, and to ban IP/word
+         */
+        function triggerSendMessage(&$obj)
+        {
+        if($_SESSION['avoid_log']) return new Object();
+        // Check the login status, login information, and permission
+        $is_logged = Context::get('is_logged');
+        $logged_info = Context::get('logged_info');
+        // In case logged in, check if it is an administrator
+        if($is_logged)
+        {
+        if($logged_info->is_admin == 'Y') return new Object();
+        }
+
+        $oFilterModel = getModel('spamfilter');
+        // Check if the IP is prohibited
+        $output = $oFilterModel->isDeniedIP();
+        if(!$output->toBool()) return $output;
+        // Check if there is a ban on the word
+        $text = $obj->title.$obj->content;
+        $output = $oFilterModel->isDeniedWord($text);
+        if(!$output->toBool()) return $output;
+        // Check the specified time
+        $output = $oFilterModel->checkLimited();
+        if(!$output->toBool()) return $output;
+        // Save a log
+        $this->insertLog();
+
+        return new Object();
+        }
+
+        /**
          * @brief IP registration
          * The registered IP address is considered as a spammer
          **/
+		
         function insertIP($ipaddress_list, $description = null) {
 			$ipaddress_list = str_replace("\r","",$ipaddress_list);
 			$ipaddress_list = explode("\n",$ipaddress_list);
